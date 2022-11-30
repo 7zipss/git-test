244cb361f1a0abf6bd80b28998af836dc5533955
ps <1323392817@qq.com>
2022-11-30 23:49:00 +0800

    idea commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3 hot-fix!");
         System.out.println("hello git4 master test!");
         System.out.println("hello git4 hot-fix!");
+        System.out.println("hello git5 test gitHub");
     }
 }
