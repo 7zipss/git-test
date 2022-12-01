848a49daede10689d75e2cd87650c3aa13f57973
ps <1323392817@qq.com>
2022-12-01 09:17:07 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4 master test!");
         System.out.println("hello git4 hot-fix!");
         System.out.println("hello git5 test push");
+        System.out.println("hello git5 test ssh push");
     }
 }
